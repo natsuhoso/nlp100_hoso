115c6508ea9db676e4531c8b42c9c56e01080113
natsuhoso <32256674+natsuhoso@users.noreply.github.com>
2019-04-08 21:59:29 +0900

    minor_change

--- data/file15.py
+++ data/file15.py
@@ -1,8 +1,11 @@
+# file15.py
 import sys
 file = sys.argv[1]
 n = int(sys.argv[2])
 
-with open(file) as r:
-    text = [(i[:-1] if i[-1] is '\n' else i) for i in  r.readlines()]
-
-print("\n".join(text[-n:]))
+temps = []
+with open('data/hightemp.txt') as r:
+    for line in r:
+        temps.append((line[:-1] if line[-1] is '\n' else line).split('\t'))
+hightemp =temps
+print("\n".join(hightemp[-n:]))
